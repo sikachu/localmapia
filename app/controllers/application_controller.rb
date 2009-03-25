e2b1084fd9310ba45f106849724794aca2232a46
Prem Sichanugrist <s@sikachu.com>
2009-03-25 22:19:13 +0700

    Deprecation remove: :secret is ignored in 2.3.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => '5fa13cf2b7db402ff2f11080d76f7aa6'
+  protect_from_forgery
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
