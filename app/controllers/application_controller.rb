0a2771c67ab839c9c481409920870bf2f3a128a5
Prem Sichanugrist <s@sikachu.com>
2009-03-25 20:34:20 +0700

    Fix the bug on authenticity token error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '5fa13cf2b7db402ff2f11080d76f7aa6'
+  protect_from_forgery :secret => '5fa13cf2b7db402ff2f11080d76f7aa6'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password, :password_confirmation
   
   protected
   
